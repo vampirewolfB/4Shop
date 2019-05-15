3093b20a836ea6d4c848c5b7e946e29eafbf564f
Bart Roos <bartjroos@gmail.com>
2019-05-15 22:55:38 +0200

    Extend seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -33,43 +33,72 @@ class DatabaseSeeder extends Seeder
         $type = new Type();
         $type->product_id = $product->id;
         $type->title = "Unisex";
-        $type->description = "Basic Hoody full zip grijs(!)";
+        $type->description = "Basic Hoody full zip (grijs, logo voor en achter)";
         $type->save();
 
-        $size = new Size();
-        $size->type_id = $type->id;
-        $size->title = "S";
-        $size->save();
-
-        $size = new Size();
-        $size->type_id = $type->id;
-        $size->title = "M";
-        $size->save();
+        $sizes = ["XS", "S", "M", "L", "XL", "XXL", "3XL"];
+        foreach ($sizes as $item)
+        {
+            $size = new Size();
+            $size->type_id = $type->id;
+            $size->title = $item;
+            $size->save();
+        }
 
         $type = new Type();
         $type->product_id = $product->id;
         $type->title = "Dames";
-        $type->description = "Basic Hoody full zip Ladies grijs(!)";
+        $type->description = "Basic Hoody full zip Ladies (grijs, logo voor en achter)";
         $type->save();
 
-        $size = new Size();
-        $size->type_id = $type->id;
-        $size->title = "XL";
-        $size->save();
-
-        $size = new Size();
-        $size->type_id = $type->id;
-        $size->title = "XXL";
-        $size->save();
+        $sizes = ["XS", "S", "M", "L", "XL"];
+        foreach ($sizes as $item)
+        {
+            $size = new Size();
+            $size->type_id = $type->id;
+            $size->title = $item;
+            $size->save();
+        }
 
         /////////////////////////////////
 
         $product = new Product();
         $product->title = 'Polo';
+        $product->description = 'Polo voor stafleden. Logo voor op de borst, handjes op de rug.';
         $product->price = 22.00;
         $product->leiding = true;
         $product->image = 'img/polo.png';
         $product->save();
+
+        $type = new Type();
+        $type->product_id = $product->id;
+        $type->title = "Unisex";
+        $type->description = "Heren polo Classic Lincoln (groen, logo voor en achter)";
+        $type->save();
+
+        $sizes = ["XS", "S", "M", "L", "XL", "XXL", "3XL"];
+        foreach ($sizes as $item)
+        {
+            $size = new Size();
+            $size->type_id = $type->id;
+            $size->title = $item;
+            $size->save();
+        }
+
+        $type = new Type();
+        $type->product_id = $product->id;
+        $type->title = "Dames";
+        $type->description = "Dames polo Classic Marion (groen, logo voor en achter)";
+        $type->save();
+
+        $sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+        foreach ($sizes as $item)
+        {
+            $size = new Size();
+            $size->type_id = $type->id;
+            $size->title = $item;
+            $size->save();
+        }
 
         /////////////////////////////////
 
@@ -80,6 +109,91 @@ class DatabaseSeeder extends Seeder
         $product->leiding = false;
         $product->image = 'img/jeugd.jpg';
         $product->save();
+
+        $type = new Type();
+        $type->product_id = $product->id;
+        $type->title = "Kindermaten";
+        $type->description = "Classic T-shirt Kids (groen, alleen logo voor!)";
+        $type->save();
+
+        $sizes = ["90/100", "110/120", "130/140", "150/160"];
+        foreach ($sizes as $item)
+        {
+            $size = new Size();
+            $size->type_id = $type->id;
+            $size->title = $item;
+            $size->save();
+        }
+
+        $type = new Type();
+        $type->product_id = $product->id;
+        $type->title = "Volwassenenmaten";
+        $type->description = "Clique basic tee (groen, alleen logo voor!)";
+        $type->save();
+
+        $sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+        foreach ($sizes as $item)
+        {
+            $size = new Size();
+            $size->type_id = $type->id;
+            $size->title = $item;
+            $size->save();
+        }
+
+        /////////////////////////////////
+
+        $product = new Product();
+        $product->title = 'T-shirt stafleden';
+        $product->description = 'T-shirt van hoge kwaliteit. Logo voor op de borst en handjes op de rug.';
+        $product->price = 16.00;
+        $product->leiding = true;
+        $product->image = 'img/staf.jpg';
+        $product->save();
+
+        $type = new Type();
+        $type->product_id = $product->id;
+        $type->title = "Unisex t/m xxl";
+        $type->description = "New Classic Heren (groen, logo voor en achter)";
+        $type->save();
+
+        $sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+        foreach ($sizes as $item)
+        {
+            $size = new Size();
+            $size->type_id = $type->id;
+            $size->title = $item;
+            $size->save();
+        }
+
+        $type = new Type();
+        $type->product_id = $product->id;
+        $type->title = "Unisex grote maten";
+        $type->description = "Clique basic tee (groen, logo voor en achter!)";
+        $type->save();
+
+        $sizes = ["3XL", "4XL"];
+        foreach ($sizes as $item)
+        {
+            $size = new Size();
+            $size->type_id = $type->id;
+            $size->title = $item;
+            $size->save();
+        }
+
+        $type = new Type();
+        $type->product_id = $product->id;
+        $type->title = "Dames";
+        $type->description = "New Classic Dames (groen, logo voor en achter)";
+        $type->save();
+
+        $sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+        foreach ($sizes as $item)
+        {
+            $size = new Size();
+            $size->type_id = $type->id;
+            $size->title = $item;
+            $size->save();
+        }
 
 
     }
